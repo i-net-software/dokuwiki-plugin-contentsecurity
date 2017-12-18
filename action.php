5d5c5ec12cfe5a82d90c086ea21de4f6c0a09ccd
i-net /// software <tools@inetsoftware.de>
2017-12-18 14:48:13 +0100

    Create the CSP nonce when initializing the plugin
    The early init phase makes the nonce available to other plugins and templates as early as possible.

--- action.php
+++ action.php
@@ -18,27 +18,82 @@ class action_plugin_contentsecurity extends DokuWiki_Action_Plugin {
      * @return void
      */
     public function register(Doku_Event_Handler $controller) {
-
-       $controller->register_hook('ACTION_HEADERS_SEND', 'FIXME', $this, 'handle_action_headers_send');
-       $controller->register_hook('TPL_METAHEADER_OUTPUT', 'FIXME', $this, 'handle_tpl_metaheader_output');
-   
+        global $dokuwikiVersion, $JSINFO;
+        $JSINFO['nonce'] = md5($dokuwikiVersion);
+        $controller->register_hook('ACTION_HEADERS_SEND', 'BEFORE', $this, 'handle_action_headers_send', $JSINFO['nonce'], 3999);
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_tpl_metaheader_output', $JSINFO['nonce'], 3999);
     }
 
     /**
-     * [Custom event handler which performs action]
+     * Send Security Values to browser.
      *
      * @param Doku_Event $event  event object by reference
      * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      * @return void
      */
+    public function handle_action_headers_send(Doku_Event &$event, $nonce) {
 
-    public function handle_action_headers_send(Doku_Event &$event, $param) {
+        $none = "'none'";
+        $self = "'self'";
+        $selfNonce = "'self' 'nonce-" . $nonce . "'";
+
+        $policies = array(
+            'default-src' => $none,
+            'script-src' => $selfNonce,
+            'style-src' => $selfNonce,
+            'connect-src' => $selfNonce,
+            'font-src' => $self,
+            'img-src' => $self . " data:",
+//            'child-src' => $self,
+//            'frame-ancestors' => $self,
+//            'plugin-types' => $none,
+        );
+
+        $headers = array(
+            "X-XSS-Protection" => "1; mode=block",
+            "X-Frame-Options" => "SAMEORIGIN",
+            "X-Content-Type-Options" => "nosniff",
+            "Strict-Transport-Security" => "max-age=63072000; includeSubDomains; preload",
+            "Content-Security-Policy" => implode('; ', array_map(
+                function ($v, $k) { return sprintf("%s %s", $k, $v); },
+                $policies,
+                array_keys($policies)
+            )),
+        );
+
+        foreach( $headers as $header => $value ) array_push($event->data, $header . ': ' . $value);
     }
 
-    public function handle_tpl_metaheader_output(Doku_Event &$event, $param) {
-    }
+    /**
+     * Add nonce to the linkl, style and script tags in html head.
+     *
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     *                           handler was registered]
+     * @return void
+     */
+    public function handle_tpl_metaheader_output(Doku_Event &$event, $nonce) {
 
+        foreach( $event->data as $type => &$content ) {
+
+            switch($type) {
+                
+                case 'style':
+                case 'script':
+                    foreach( $content as &$style ) {
+                        $style['nonce'] = $nonce;
+                    }
+                    break;
+                case 'link':
+                    foreach( $content as &$link ) {
+                        if ( $link['rel'] == 'stylesheet' ) {
+                            $link['nonce'] = $nonce;
+                        }
+                    }
+            }
+        }
+    }
 }
 
 // vim:ts=4:sw=4:et:
